81fb253192ca1e8024399b39ebae369402c0310c
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-05 17:43:26 +1200

    nameplate responsive

--- src/app/components/NamePlate.tsx
+++ src/app/components/NamePlate.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export function NamePlate() {
     return (
-        <section className="w-full h-36 flex content-center items-center">
-            <div className="justify-center px-72 py-3 w-fit h-fit bg-[#2B417B] border-8 rounded-xl">
+        <section className="w-full h-36 flex content-center justify-center">
+            <div className="justify-center  px-[15%] py-3 w-fit h-fit bg-[#2B417B] border-8 rounded-xl">
                 <h1 className="text-3xl font-semibold font-roboto whitespace-nowrap drop-shadow-md">
                     Nevada Kingi-Whata
                 </h1>
