ac61ba55725466a38711ba0b227fa189ba5a54b7
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-08 01:00:23 +1200

    contact sect.

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import GithubIcon from '../GithubIcon';
+import LinkedInIcon from '../LinkedInIcon';
+import PSIcon from '../PSIcon';
 
 export default function Contact({ show }: { show: boolean }) {
     if (!show) return;
@@ -12,43 +15,50 @@ export default function Contact({ show }: { show: boolean }) {
                                 className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
                                 font-roboto font-bold"
                             >
-                                EMAIL
+                                DETAILS
                             </h2>
                         </div>
 
                         <h2 className="text-3xl whitespace-nowrap font-roboto font-semibold text-shadow">
-                            kingiwhata@gmail.com
+                            Let's Connect!
                         </h2>
                     </div>
+                </div>
 
+                <div className="flex flex-col gap-4 h-full pl-20 pr-20">
+                    <div className="w-fit flex h-full justify-start flex-col gap-4">
+                        <p
+                            className="
+                                font-roboto text-xl text-shadow"
+                        >
+                            If you want to get in touch, talk to me about a
+                            project, game together or just want to say hi,
+                            message me through my socials/email below.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row w-full pl-20 pr-20 gap-4  content-end justify-end">
                     <div className="flex flex-col  gap-4 content-center items-center justify-center h-full ">
-                        <div className="w-full pl-24 pt-4 flex h-full justify-start flex-col gap-4">
-                            <div className="absolute flex flex-col gap-4 content-center items-center justify-center">
-                                <h2
-                                    className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
-                                font-roboto font-extrabold"
-                                >
-                                    LINKEDIN
-                                </h2>
+                        <div className="w-full pt-4 flex h-full justify-start flex-col gap-4">
+                            <div className="h-14 w-14">
+                                <LinkedInIcon />
                             </div>
-
-                            <h2 className="text-3xl font-roboto font-semibold text-shadow">
-                                Nevada
-                            </h2>
                         </div>
                     </div>
 
                     <div className="flex flex-col  gap-4 content-center items-center justify-center h-full ">
-                        <div className="w-full pl-24 pt-4 flex h-full justify-start flex-col gap-4">
-                            <div className="absolute flex flex-col gap-4 content-center items-center justify-center">
-                                <h2 className="relative bottom-[2.2rem] leading-6 text-outline text-lg font-roboto font-extrabold">
-                                    GITHUB
-                                </h2>
+                        <div className="w-full pt-4 flex h-full justify-start flex-col gap-4">
+                            <div className="h-14 w-14">
+                                <GithubIcon />
                             </div>
+                        </div>
+                    </div>
 
-                            <h2 className="text-3xl font-roboto font-semibold text-shadow">
-                                Vadaku
-                            </h2>
+                    <div className="flex flex-col  gap-4 content-center items-center justify-center h-full ">
+                        <div className="w-full pt-4 flex h-full justify-start flex-col gap-4">
+                            <div className="h-14 w-14">
+                                <PSIcon />
+                            </div>
                         </div>
                     </div>
                 </div>
