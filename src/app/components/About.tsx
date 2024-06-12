d0b47243010b774d278d5b0943c361ccedc0c318
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-12 19:47:10 +1200

    experience section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,7 +5,7 @@ export default function About({ show }: { show: boolean }) {
     return (
         <div className="scale-in flex w-full rounded-xl justify-start items-center md:items-start flex-col gap-4 md:h-full h-64 bg-[#2B417B] outline outline-8 outline-[#CFD3DE]">
             <div className="flex flex-row ">
-                <div className="w-full pl-20 pr-20 pt-4 flex  items-center md:items-start h-full flex-col gap-4">
+                <div className="w-full md:px-20 px-8 pt-4 flex  items-center md:items-start h-full flex-col gap-4">
                     <div className="absolute flex flex-col ">
                         <h2
                             className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
@@ -20,8 +20,8 @@ export default function About({ show }: { show: boolean }) {
                     </h2>
                 </div>
 
-                <div className="flex-col gap-4  items-center md:flex hidden justify-center h-full ">
-                    <div className="w-full pl-24 pt-4 flex h-full justify-start flex-col gap-4">
+                <div className="flex-col gap-4  items-center lg:flex hidden justify-center h-full ">
+                    <div className="w-full pl-8 lg:pl-24 pt-4 flex h-full justify-start flex-col gap-4">
                         <div className="absolute flex flex-col gap-4  items-center justify-center">
                             <h2
                                 className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
@@ -37,7 +37,7 @@ export default function About({ show }: { show: boolean }) {
                     </div>
                 </div>
 
-                <div className="hidden flex-col  gap-4  items-center lg:flex  justify-center  h-full ">
+                <div className="hidden flex-col  gap-4  items-center xl:flex  justify-center  h-full ">
                     <div className="w-full pl-24 pt-4 flex h-full justify-start flex-col gap-4">
                         <div className="absolute flex flex-col gap-4  items-center justify-center">
                             <h2 className="relative bottom-[2.2rem] leading-6 text-outline text-lg font-roboto font-extrabold">
@@ -53,7 +53,7 @@ export default function About({ show }: { show: boolean }) {
                 </div>
 
                 <div className="hidden lg:flex flex-col  gap-4  items-center justify-center h-full ">
-                    <div className="w-full pl-8 pt-4 flex h-full justify-start flex-col gap-4">
+                    <div className="w-full xl:pl-8 pl-24 pt-4 flex h-full justify-start flex-col gap-4">
                         <div className="absolute flex flex-col gap-4  items-center justify-center">
                             <h2
                                 className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
@@ -77,14 +77,16 @@ export default function About({ show }: { show: boolean }) {
                         className="
                                 font-roboto text-clamp text-lg md:text-xl text-shadow md:text-left text-center"
                     >
-                        Welcome to my portfolio, Software engineer with a
-                        passion for developing innovative solutions to complex
-                        problems. With expertise in full-stack development,
-                        cloud technologies, and agile methodologies, I create
-                        efficient, scalable, and user-friendly applications. My
-                        experience spans across various projects, from building
-                        robust e-commerce platforms to designing real-time chat
-                        applications and developing machine learning pipelines.
+                        Software engineer with two and a half years experience,
+                        specializing in blockchain and front-end development,
+                        with extensive experience in Typescript, HTML5 and CSS3
+                        as well as proficiency in frameworks like Svelte, React
+                        and NextJS. I am passionate about creating intuitive and
+                        responsive web applications that provide seamless user
+                        experiences across all devices. I thrive on continuous
+                        learning and innovation, consistently seeking out new
+                        challenges, while also keeping up with changes within
+                        the ever evolving tech industry.
                     </p>
                 </div>
             </div>
