e8d2885223575dbbb7a0ed1c5540ce740391688b
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-07-09 12:50:56 +1200

    break command to components

---
+++ src/app/components/Command.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Pointer from '../../../public/Pointer';
+import TitleSection from './Title';
+
+export function CommandPanel({ commands, focusedIndex, handleMouseEnter }) {
+    return (
+        <div className="flex flex-col bg-[#2B417B] h-auto md:h-full gap-4 items-center outline-[#CFD3DE] justify-center w-full md:w-1/3 outline outline-8 rounded-xl">
+            <div className="w-full md:px-24 pt-4 md:pb-0 pb-4 flex justify-center h-full md:justify-start flex-row md:flex-col gap-4 z-10">
+                <TitleSection title="COMMAND" />
+                {commands.map((command: string, index: number) => (
+                    <CommandItem
+                        key={index}
+                        index={index}
+                        command={command}
+                        focusedIndex={focusedIndex}
+                        handleMouseEnter={handleMouseEnter}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function CommandItem({ index, command, focusedIndex, handleMouseEnter }) {
+    return (
+        <div className="flex flex-row relative">
+            <div className="md:flex hidden">
+                <div
+                    className="absolute -left-16"
+                    style={{
+                        display: index === focusedIndex ? 'flex' : 'none',
+                    }}
+                >
+                    <Pointer filterID={index} />
+                </div>
+            </div>
+            <h2
+                className="md:text-3xl text-lg h-fit font-roboto font-semibold text-shadow"
+                onMouseEnter={() => handleMouseEnter(index)}
+            >
+                {command}
+            </h2>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import About from './components/About';
 import Center from './components/Center';
+import { CommandPanel } from './components/Command';
 import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import { NamePlate } from './components/NamePlate';
-import Pointer from '../../public/Pointer';
 import { useState, useEffect, useCallback } from 'react';
 
 export default function Home() {
@@ -72,46 +72,24 @@ export default function Home() {
                 />
             </section>
             <section className="md:h-[17rem] flex-none h-fit w-full 2xl:w-3/4 px-2 flex flex-col-reverse md:flex-row  my-2 gap-[1.05rem]">
-                <div className="flex flex-col bg-[#2B417B]  h-auto md:h-full gap-4  items-center outline-[#CFD3DE] justify-center  w-full md:w-1/3 outline outline-8 rounded-xl">
-                    <div className="w-full md:px-24 pt-4 md:pb-0 pb-4 flex justify-center h-full md:justify-start flex-row md:flex-col gap-4 z-10">
-                        <div className="absolute flex flex-col items-center justify-center">
-                            <h2
-                                className="relative bottom-[2.2rem] leading-6 text-outline text-lg 
-                                font-roboto font-bold"
-                            >
-                                COMMAND
-                            </h2>
-                        </div>
-                        {commands.map((command, index) => (
-                            <div className="flex flex-row relative" key={index}>
-                                <div className="md:flex hidden">
-                                    <div
-                                        className="absolute -left-16 md:flex hidden"
-                                        style={{
-                                            display:
-                                                index === focusedIndex
-                                                    ? 'flex'
-                                                    : 'none',
-                                        }}
-                                    >
-                                        <Pointer filterID={index} key={index} />
-                                    </div>
-                                </div>
-                                <h2
-                                    className="md:text-3xl text-lg h-fit font-roboto font-semibold text-shadow"
-                                    onMouseEnter={() => handleMouseEnter(index)}
-                                >
-                                    {command}
-                                </h2>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <About show={commands[focusedIndex] == 'About'} />
-                <Experience show={commands[focusedIndex] == 'Experience'} />
-                <Contact show={commands[focusedIndex] == 'Contact'} />
-                <Projects show={commands[focusedIndex] == 'Projects'} />
+                <CommandPanel
+                    commands={commands}
+                    focusedIndex={focusedIndex}
+                    handleMouseEnter={handleMouseEnter}
+                />
+                <Content focusedCommand={commands[focusedIndex]} />
             </section>
         </main>
+    );
+}
+
+function Content({ focusedCommand }) {
+    return (
+        <>
+            <About show={focusedCommand === 'About'} />
+            <Experience show={focusedCommand === 'Experience'} />
+            <Contact show={focusedCommand === 'Contact'} />
+            <Projects show={focusedCommand === 'Projects'} />
+        </>
     );
 }
